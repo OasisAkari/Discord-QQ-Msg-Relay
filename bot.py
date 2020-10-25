5f06ec88875073f5e5404160358beb97c017b8a2
yzhh <OasisAkari@users.noreply.github.com>
2020-10-25 19:21:22 +0800

    update

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
 import re
+from configparser import ConfigParser
+from os.path import abspath
 
 import aiohttp
 import discord
@@ -8,12 +10,10 @@ from discord import Webhook, AsyncWebhookAdapter
 from graia.application import GraiaMiraiApplication, Session
 from graia.application.group import Group, Member
 from graia.application.message.chain import MessageChain
-from graia.application.message.elements.internal import Plain, Image, FlashImage, Source, At, App, AtAll, Xml, Json, \
+from graia.application.message.elements.internal import Plain, Image, FlashImage, At, App, AtAll, Xml, Json, \
     Poke, Voice, Quote, Face
 from graia.application.message.elements.internal import UploadMethods
 from graia.broadcast import Broadcast
-from configparser import ConfigParser
-from os.path import abspath
 
 cp = ConfigParser()
 cp.read(abspath("./config.cfg"))
@@ -25,39 +25,80 @@ bcc = Broadcast(loop=loop)
 app = GraiaMiraiApplication(
     broadcast=bcc,
     connect_info=Session(
-        host=cp.get(section, 'mahlink'),
-        authKey=cp.get(section, 'mahauth'),
+        host=cp.get(section, 'mah_link'),
+        authKey=cp.get(section, 'mah_auth'),
         account=cp.get(section, 'qq'),
         websocket=True
     )
 )
 
-targetqqgroup = int(cp.get(section, 'qqgroup'))
-webhooklink = cp.get(section, 'webhooklink')
+target_qqgroup = int(cp.get(section, 'qqgroup'))
+webhook_link = cp.get(section, 'webhook_link')
+debug = bool(cp.get(section, 'debug'))
+if debug == True:
+    debug_webhook_link = cp.get(section, 'debug_webhook_link')
+else:
+    debug_webhook_link = None
+
+
+async def dc_debug_webhook(message, username, avatar_url=None):
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(5)) as session:
+        webhook = Webhook.from_url(debug_webhook_link
+                                   ,
+                                   adapter=AsyncWebhookAdapter(session))
+        await webhook.send(message, username=username,
+                           avatar_url=avatar_url)
+
+
+@bcc.receiver("ApplicationLaunched")
+async def runprompt():
+    await dc_debug_webhook(f'互联QQ侧机器人已启动。', f'[INFO] QQBOT',
+                           'https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
+
 
 async def qq_recv_msg(websocket, path):
     while True:
         try:
             recv_text = await websocket.recv()
+            if debug == True:
+                await dc_debug_webhook(f'收到一条消息：`{recv_text}`，开始重建消息链。', f'[INFO] Websocket_QQ',
+                                       avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
             msgchain = MessageChain.create([])
             text = recv_text
             text = re.sub('\[\<.*:.*\>\]', '', text)
-            text = re.split(r'(@\[QQ: .*?\].*#0000)',text)
+            text = re.split(r'(@\[QQ: .*?\].*#0000)', text)
             for ele in text:
-                matele = re.match(r'@\[QQ: (.*?)]',ele)
+                matele = re.match(r'@\[QQ: (.*?)]', ele)
                 if matele:
-                    print('t')
                     msgchain = msgchain.plusWith([At(int(matele.group(1)))])
                 else:
                     msgchain = msgchain.plusWith([Plain(ele)])
+            try:
+                await app.sendGroupMessage(target_qqgroup, msgchain)
+                if debug == True:
+                    await dc_debug_webhook(f'成功将`{str(msgchain)}`发送至{int(target_qqgroup)}！', f'[OK] {target_qqgroup}',
+                                           avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
+            except Exception as e:
+                if debug == True:
+                    await dc_debug_webhook(f'`{str(msgchain)}`发送至{int(target_qqgroup)}时抛出了错误：\n{str(e)}',
+                                           f'[ERROR] {target_qqgroup}',
+                                           avatar_url='https://discordapp.com/assets/8becd37ab9d13cdfe37c08c496a9def3.svg')
             textre = re.findall(r'\[\<.*?:.*?\>\]', recv_text)
             for elements in textre:
                 a = re.match(r'\[\<ImageURL:(.*)\>\]', elements)
                 if a:
                     msgchain2 = msgchain.create([Image.fromNetworkAddress(url=a.group(1), method=UploadMethods.Group)])
-                    await app.sendGroupMessage(targetqqgroup, msgchain2)
-            print(msgchain)
-            await app.sendGroupMessage(targetqqgroup, msgchain)
+                    try:
+                        await app.sendGroupMessage(target_qqgroup, msgchain2)
+                        if debug == True:
+                            await dc_debug_webhook(f'成功将`{str(msgchain2)}`发送至{int(target_qqgroup)}！',
+                                                   f'[OK] {target_qqgroup}',
+                                                   avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
+                    except Exception as e:
+                        if debug == True:
+                            await dc_debug_webhook(f'`{str(msgchain)}`发送至{int(target_qqgroup)}时抛出了错误：\n{str(e)}',
+                                                   f'[ERROR] {target_qqgroup}',
+                                                   avatar_url='https://discordapp.com/assets/8becd37ab9d13cdfe37c08c496a9def3.svg')
         except websockets.exceptions.ConnectionClosedOK:
             pass
 
@@ -66,9 +107,11 @@ async def dc_recv_msg(websocket, path):
     while True:
         try:
             recv_text = await websocket.recv()
+            if debug == True:
+                await dc_debug_webhook(f'收到一条消息：`{recv_text}`，开始重建消息链。', f'[INFO] Websocket_DC')
             recv_text = recv_text.split('!:!:!:wqwqw!qwqwq')
-            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(5)) as session:
-                webhook = Webhook.from_url(webhooklink
+            async with aiohttp.ClientSession() as session:
+                webhook = Webhook.from_url(webhook_link
                                            ,
                                            adapter=AsyncWebhookAdapter(session))
                 qqavatarbase = 'https://ptlogin2.qq.com/getface?appid=1006102&imgtype=3&uin=' + recv_text[0]
@@ -76,36 +119,55 @@ async def dc_recv_msg(websocket, path):
                     try:
                         qqavatarlink = re.match(r'pt.setHeader\({".*?":"(https://thirdqq.qlogo.cn/.*)"}\)',
                                                 await qlink.text())
+                        qqavatarlink = qqavatarlink.group(1)
                     except Exception:
-                        qqavatarlink = 'https://im.qq.com/assets/images/logo.png'
-                print(qqavatarlink)
-                await webhook.send(recv_text[2], username=f'[QQ: {recv_text[0]}] {recv_text[1]}',
-                                   avatar_url=qqavatarlink.group(1),
-                                   allowed_mentions=discord.AllowedMentions(everyone=True))
+                        qqavatarlink = None
+                try:
+                    await webhook.send(recv_text[2], username=f'[QQ: {recv_text[0]}] {recv_text[1]}',
+                                       avatar_url=qqavatarlink,
+                                       allowed_mentions=discord.AllowedMentions(everyone=True))
+                    if debug == True:
+                        await dc_debug_webhook(f'成功将一条消息发送至Discord：`{recv_text[2]}`', f'[OK] Discord Webhook')
+                except Exception as e:
+                    if debug == True:
+                        await dc_debug_webhook(f'`{str(recv_text)}`发送时抛出了错误：\n{str(e)}',
+                                               f'[ERROR] Discord Webhook',
+                                               avatar_url='https://discordapp.com/assets/8becd37ab9d13cdfe37c08c496a9def3.svg')
         except websockets.exceptions.ConnectionClosedOK:
             pass
 
 
 @bcc.receiver("ApplicationLaunched", priority=1)
 async def start_websocket():
-    await websockets.serve(dc_recv_msg, '127.0.0.1', int(cp.get(section, 'websocketport1')))
+    await websockets.serve(dc_recv_msg, '127.0.0.1', int(cp.get(section, 'websocket_dc_port')))
 
 
 @bcc.receiver("ApplicationLaunched", priority=2)
 async def start_websocket():
-    await websockets.serve(qq_recv_msg, '127.0.0.1', int(cp.get(section, 'websocketport2')))
+    await websockets.serve(qq_recv_msg, '127.0.0.1', int(cp.get(section, 'websocket_qq_port')))
 
 
 async def sendmsg(message):
-    async with websockets.connect('ws://127.0.0.1:'+cp.get(section, 'websocketport1')) as websocket:
-        a = await websocket.send('!:!:!:wqwqw!qwqwq'.join(message))
-        print(a)
+    async with websockets.connect('ws://127.0.0.1:' + cp.get(section, 'websocket_dc_port')) as websocket:
+        try:
+            message = '!:!:!:wqwqw!qwqwq'.join(message)
+            await websocket.send(message)
+            if debug == True:
+                await dc_debug_webhook(f'成功发送一条消息到Websocket_DC：`{message}`', f'[OK] QQGroup -> Websocket_DC')
+        except Exception as e:
+            if debug == True:
+                await dc_debug_webhook(f'`{str(message)}`发送时抛出了错误：\n{str(e)}',
+                                       f'[ERROR] QQGroup -x Websocket_DC',
+                                       avatar_url='https://discordapp.com/assets/8becd37ab9d13cdfe37c08c496a9def3.svg')
 
 
 @bcc.receiver("GroupMessage", priority=3)
 async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if group.id == targetqqgroup:
+    if group.id == target_qqgroup:
         if message.asDisplay()[0:2] != '//':
+            if debug == True:
+                await dc_debug_webhook(f'收到消息链`{str(message)}`，开始转换消息链。', f'[INFO] {group.id}',
+                                       avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
             msglist = []
             quotes = message.get(Quote)
             for quote in quotes:
@@ -147,10 +209,16 @@ async def group_message_handler(app: GraiaMiraiApplication, message: MessageChai
             flashimages = message.get(FlashImage)
             for flashimage in flashimages:
                 msglist.append('[闪照]')
-            msglist = str(member.id), member.name, '\n'.join(msglist)
-
-            print(msglist)
+            allmsg = '\n'.join(msglist)
+            if debug == True:
+                await dc_debug_webhook(f'消息链转换完毕：\n`{str(message)}` -> {str(allmsg)}。', f'[INFO] {group.id}',
+                                       avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
+            msglist = str(member.id), member.name, allmsg
             await sendmsg(msglist)
+        else:
+            if debug == True:
+                await dc_debug_webhook('有一条消息触发转发过滤，此消息已过滤', f'[INFO] {group.id}',
+                                       avatar_url='https://cdn.discordapp.com/avatars/700205918918541333/c039f234d1796106fb989bcb0e3fe735.png')
 
 
 app.launch_blocking()
--- discordbot.py
+++ discordbot.py
@@ -1,41 +1,64 @@
-import discord
-import websockets
-import re
 import asyncio
+import re
 from configparser import ConfigParser
 from os.path import abspath
+
+import aiohttp
+import discord
+import websockets
+from discord import Webhook, AsyncWebhookAdapter
+
 client = discord.Client()
 
 cp = ConfigParser()
 cp.read(abspath("./config.cfg"))
 section = cp.sections()[0]
-channelid = int(cp.get(section, 'dcchannel'))
-bottoken = cp.get(section, 'dcbottoken')
+channelid = int(cp.get(section, 'dc_channel'))
+bottoken = cp.get(section, 'dc_bottoken')
+debug = bool(cp.get(section, 'debug'))
+if debug == True:
+    debug_webhook_link = cp.get(section, 'debug_webhook_link')
+else:
+    debug_webhook_link = None
+
+
+async def dc_debug_webhook(message, username, avatar_url=None):
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(5)) as session:
+        webhook = Webhook.from_url(debug_webhook_link
+                                   ,
+                                   adapter=AsyncWebhookAdapter(session))
+        await webhook.send(message, username=username,
+                           avatar_url=avatar_url)
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    if debug == True:
+        await dc_debug_webhook(f'互联DC侧机器人已启动。', f'[INFO] DCBOT')
+
 
 @client.event
 async def on_message(message):
-    print(message)
-    print(message.channel.id)
-    print(message.author)
     botfliter = re.match(r'^\[QQ: (.*?)\].*?#0000$', str(message.author))
     if not botfliter:
         if message.channel.id == channelid:
-            async with websockets.connect('ws://127.0.0.1:'+cp.get(section, 'websocketport2')) as websocket:
+            if debug == True:
+                await dc_debug_webhook(
+                    f'从频道{message.channel.id}中接收到一条消息：`{message.author}: {message.content + str(message.embeds)}`，开始消息链转换。',
+                    f'[INFO] {message.channel.id}')
+            async with websockets.connect('ws://127.0.0.1:' + cp.get(section, 'websocket_qq_port')) as websocket:
                 messages = message.content
                 if messages[0:2] != '//':
                     print(messages[0:2])
                     emojis = re.findall(r'<:.*?:.*?>', messages)
                     print(emojis)
                     for emoji in emojis:
-                        a = re.match(r'\<:.*?:(.*?)\>',emoji)
+                        a = re.match(r'\<:.*?:(.*?)\>', emoji)
                         print(a.group(1))
                         if a:
-                            b = 'https://cdn.discordapp.com/emojis/'+a.group(1)
-                            messages = re.sub(emoji, f'[<ImageURL:{b}>]',messages)
+                            b = 'https://cdn.discordapp.com/emojis/' + a.group(1)
+                            messages = re.sub(emoji, f'[<ImageURL:{b}>]', messages)
                     print(messages)
                     for embed in message.embeds:
                         messages += embed
@@ -43,5 +66,21 @@ async def on_message(message):
                         messages += f'[<ImageURL:{message.attachments[0].proxy_url}>]'
                     except Exception:
                         pass
-                    await websocket.send(f'{message.author}: {messages}')
+                    try:
+                        messages = f'{message.author}: {messages}'
+                        await websocket.send(messages)
+                        if debug == True:
+                            await dc_debug_webhook(
+                                f'成功发送一条消息到Websocket_QQ：{messages}', f'[OK] DCChannel -> Websocket_QQ')
+                    except Exception as e:
+                        if debug == True:
+                            await dc_debug_webhook(f'`{str(message)}`发送时抛出了错误：\n{str(e)}',
+                                                   f'[ERROR] DCChannel -x Websocket_QQ',
+                                                   avatar_url='https://discordapp.com/assets/8becd37ab9d13cdfe37c08c496a9def3.svg')
+                else:
+                    await dc_debug_webhook(
+                        '此消息触发转发过滤，已过滤'
+                        f'[INFO] {message.channel.id}')
+
+
 asyncio.create_task(client.run(bottoken))
