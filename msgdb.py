5c2ea374cb085ed1cde1bae3a9bd1f1849599ae1
yzhh <OasisAkari@users.noreply.github.com>
2020-11-07 17:21:55 +0800

    and this

--- msgdb.py
+++ msgdb.py
@@ -30,3 +30,22 @@ def writeqqmsg(msgid, msg):
     c = conn.cursor()
     c.execute("INSERT INTO MSG (ID, MSG) VALUES (?, ?)", (msgid, msg))
     conn.commit()
+
+def writedcuser(dcname, id):
+    dbpath = os.path.abspath('./dcname.db')
+    if not os.path.exists(dbpath):
+        createdb = open(dbpath,'w')
+        createdb.close()
+        conn = sqlite3.connect(dbpath)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE DCNAME
+               (NAME TEXT PRIMARY KEY     NOT NULL,
+               ID           TEXT    NOT NULL);''')
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO DCNAME (NAME, ID) VALUES (?, ?)", (dcname, id))
+    except Exception:
+        c.execute("DElETE FROM DCNAME WHERE NAME =?", (dcname,))
+        c.execute("INSERT INTO DCNAME (NAME, ID) VALUES (?, ?)", (dcname, id))
+    conn.commit()
