e1d15c2cc47a92aa2ae6a7011a347f1a87937a0d
yzhh <OasisAkari@users.noreply.github.com>
2021-02-16 13:33:23 +0800

    test

--- bot.py
+++ bot.py
@@ -97,8 +97,8 @@ async def ready():
 async def recv_msg():
     async with websockets.connect('ws://127.0.0.1:' + websocket_port) as websocket:
         while True:
-            eventlet.monkey_patch()
             try:
+                eventlet.monkey_patch()
                 with eventlet.Timeout(30):
                     try:
                         recv_text = await websocket.recv()
@@ -136,7 +136,6 @@ async def recv_msg():
                                     if debug == True:
                                         helper.writeqqmsg(msgid, j['Text'])
                                     return msgid
-                                eventlet.monkey_patch()
                                 try:
                                     with eventlet.Timeout(15):
                                         msgid = await sendmsg(j, msgchain)
@@ -249,169 +248,184 @@ async def recv_msg():
 
 @bcc.receiver("GroupMessage")
 async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if group.id == target_qqgroup:
-        if message.asDisplay()[0:2] != '//':
-            dst = {}
-            print(message)
-            msglist = []
-            newquotetarget = None
-            quotes = message.get(Quote)
-            for quote in quotes:
-                Quotet = {}
-                senderId = quote.senderId
-                orginquote = quote.origin.asDisplay()
-                if senderId != qq:
-                    Quotet['From'] = 'QQ'
-                    try:
-                        getnickname = await app.getMember(target_qqgroup, senderId)
-                        getnickname = re.sub(r'(\*|_|`|~~)', r'\\\1', getnickname.name)
-                        Quotet['Name'] = getnickname
-                    except Exception:
-                        Quotet['Name'] = senderId
-                else:
-                    Quotet['From'] = 'Discord'
-                    newquotetargetre = re.match(r'(.*?):.*', orginquote)
-                    if newquotetargetre:
-                        newquotetarget = newquotetargetre.group(1)
-                        Quotet['Name'] = newquotetarget
-                        orginquote = re.sub(r'.*?:', '', orginquote)
-                    else:
-                        Quotet['Name'] = ''
-                orginquote = re.sub('\r', '\n', orginquote)
-                Quotet['MID'] = quote.id
-                Quotet['Text'] = orginquote
-                try:
-                    time = quote.origin[Source][0].time.astimezone(timezone(timedelta(hours=8)))
-                except:
-                    time = ''
-                time = re.sub(r'\+.*', '', str(time))
-                Quotet['Time'] = time
-                dst['Quote'] = Quotet
-            ats = message.get(At)
-            for at in ats:
-                atId = at.target
-                atdis = f'@[QQ: {atId}]'
-                if atId == qq:
-                    if newquotetarget != None:
-                        mat = re.match(r'.*\((.*)\)', newquotetarget)
-                        if mat:
-                            newquotetarget = mat.group(1)
+    try:
+        eventlet.monkey_patch()
+        with eventlet.Timeout(15):
+            if group.id == target_qqgroup:
+                if message.asDisplay()[0:2] != '//':
+                    dst = {}
+                    print(message)
+                    msglist = []
+                    newquotetarget = None
+                    quotes = message.get(Quote)
+                    for quote in quotes:
+                        Quotet = {}
+                        senderId = quote.senderId
+                        orginquote = quote.origin.asDisplay()
+                        if senderId != qq:
+                            Quotet['From'] = 'QQ'
+                            try:
+                                getnickname = await app.getMember(target_qqgroup, senderId)
+                                getnickname = re.sub(r'(\*|_|`|~~)', r'\\\1', getnickname.name)
+                                Quotet['Name'] = getnickname
+                            except Exception:
+                                Quotet['Name'] = senderId
+                        else:
+                            Quotet['From'] = 'Discord'
+                            newquotetargetre = re.match(r'(.*?):.*', orginquote)
+                            if newquotetargetre:
+                                newquotetarget = newquotetargetre.group(1)
+                                Quotet['Name'] = newquotetarget
+                                orginquote = re.sub(r'.*?:', '', orginquote)
+                            else:
+                                Quotet['Name'] = ''
+                        orginquote = re.sub('\r', '\n', orginquote)
+                        Quotet['MID'] = quote.id
+                        Quotet['Text'] = orginquote
                         try:
-                            c = helper.connect_db('./dcname.db')
-                            cc = c.execute("SELECT * FROM DCNAME WHERE NAME=?", (newquotetarget,))
-                            for x in cc:
-                                print(x)
-                                newquotetarge = f'<@!{x[1]}>'
+                            time = quote.origin[Source][0].time.astimezone(timezone(timedelta(hours=8)))
                         except:
-                            newquotetarge = f'@{newquotetarget}'
-                        atdis = newquotetarge
-                else:
-                    try:
-                        getnickname = await app.getMember(target_qqgroup, atId)
-                        getnickname = re.sub(r'(\*|_|`|~~)', r'\\\1', getnickname.name)
-                        atdis = f'{atdis} {getnickname}'
-                    except Exception:
-                        pass
-                if atdis not in msglist:
-                    msglist.append(atdis)
-            atalls = message.get(AtAll)
-            for atall in atalls:
-                msglist.append('@全体成员')
-            msgs = message.get(Plain)
-            for msg in msgs:
-                if msg.text != ' ':
-                    msglist.append(msg.text)
-            imgs = message.get(Image)
-            for img in imgs:
-                msglist.append(img.url)
-            faces = message.get(Face)
-            for face in faces:
-                if face_link != 'None':
-                    msglist.append(f'{face_link}s{face.faceId}.gif')
-                else:
-                    msglist.append(f'[表情{face.faceId}]')
-            xmls = message.get(Xml)
-            for xml in xmls:
-                msglist.append(f'[Xml消息]\n```\n{xml}\n```')
-            jsons = message.get(Json)
-            for jsonn in jsons:
-                msglist.append(f'[Json消息]\n```\n{jsonn}\n```')
-            apps = message.get(App)
-            for appp in apps:
-                msglist.append(f'[App消息]\n```\n{appp}\n```')
-            pokes = message.get(Poke)
-            for poke in pokes:
-                msglist.append('[戳一戳]')
-            voices = message.get(Voice)
-            for voice in voices:
-                msglist.append('[语音]')
-            flashimages = message.get(FlashImage)
-            for flashimage in flashimages:
-                if debug == True:
-                    await helper.dc_debug_webhook(debug_webhook_link, f'{member.id} 发送了一条闪照 {flashimage.url}', '[QQ]')
-            allmsg = '\n'.join(msglist)
-            if debug == True:
-                helper.writeqqmsg(message[Source][0].id, allmsg)
-            dst['Type'] = 'Discord'
-            dst['UID'] = str(member.id)
-            dst['Name'] = member.name
-            dst['MID'] = str(message[Source][0].id)
-            dst['Text'] = allmsg
-            j = json.dumps(dst)
-            await helper.sendtoWebsocket(websocket_port, j)
+                            time = ''
+                        time = re.sub(r'\+.*', '', str(time))
+                        Quotet['Time'] = time
+                        dst['Quote'] = Quotet
+                    ats = message.get(At)
+                    for at in ats:
+                        atId = at.target
+                        atdis = f'@[QQ: {atId}]'
+                        if atId == qq:
+                            if newquotetarget != None:
+                                mat = re.match(r'.*\((.*)\)', newquotetarget)
+                                if mat:
+                                    newquotetarget = mat.group(1)
+                                try:
+                                    c = helper.connect_db('./dcname.db')
+                                    cc = c.execute("SELECT * FROM DCNAME WHERE NAME=?", (newquotetarget,))
+                                    for x in cc:
+                                        print(x)
+                                        newquotetarge = f'<@!{x[1]}>'
+                                except:
+                                    newquotetarge = f'@{newquotetarget}'
+                                atdis = newquotetarge
+                        else:
+                            try:
+                                getnickname = await app.getMember(target_qqgroup, atId)
+                                getnickname = re.sub(r'(\*|_|`|~~)', r'\\\1', getnickname.name)
+                                atdis = f'{atdis} {getnickname}'
+                            except Exception:
+                                pass
+                        if atdis not in msglist:
+                            msglist.append(atdis)
+                    atalls = message.get(AtAll)
+                    for atall in atalls:
+                        msglist.append('@全体成员')
+                    msgs = message.get(Plain)
+                    for msg in msgs:
+                        if msg.text != ' ':
+                            msglist.append(msg.text)
+                    imgs = message.get(Image)
+                    for img in imgs:
+                        msglist.append(img.url)
+                    faces = message.get(Face)
+                    for face in faces:
+                        if face_link != 'None':
+                            msglist.append(f'{face_link}s{face.faceId}.gif')
+                        else:
+                            msglist.append(f'[表情{face.faceId}]')
+                    xmls = message.get(Xml)
+                    for xml in xmls:
+                        msglist.append(f'[Xml消息]\n```\n{xml}\n```')
+                    jsons = message.get(Json)
+                    for jsonn in jsons:
+                        msglist.append(f'[Json消息]\n```\n{jsonn}\n```')
+                    apps = message.get(App)
+                    for appp in apps:
+                        msglist.append(f'[App消息]\n```\n{appp}\n```')
+                    pokes = message.get(Poke)
+                    for poke in pokes:
+                        msglist.append('[戳一戳]')
+                    voices = message.get(Voice)
+                    for voice in voices:
+                        msglist.append('[语音]')
+                    flashimages = message.get(FlashImage)
+                    for flashimage in flashimages:
+                        if debug == True:
+                            await helper.dc_debug_webhook(debug_webhook_link, f'{member.id} 发送了一条闪照 {flashimage.url}', '[QQ]')
+                    allmsg = '\n'.join(msglist)
+                    if debug == True:
+                        helper.writeqqmsg(message[Source][0].id, allmsg)
+                    dst['Type'] = 'Discord'
+                    dst['UID'] = str(member.id)
+                    dst['Name'] = member.name
+                    dst['MID'] = str(message[Source][0].id)
+                    dst['Text'] = allmsg
+                    j = json.dumps(dst)
+                    await helper.sendtoWebsocket(websocket_port, j)
+    except eventlet.TimeoutError:
+        traceback.print_exc()
 
 
 @bcc.receiver("GroupRecallEvent")
 async def revokeevent(event: GroupRecallEvent):
     print(event)
-    if event.group.id == target_qqgroup:
-        dst = {}
-        if event.authorId != qq:
-            dst['Type'] = 'QQrecall'
-        else:
-            dst['Type'] = 'QQrecallI'
-        dst['MID'] = event.messageId
-        dst['UID'] = event.authorId
-        j = json.dumps(dst)
-        await helper.sendtoWebsocket(websocket_port, j)
-        if debug:
-            print(event.authorId)
-            try:
-                c = helper.connect_db('./qqmsg.db')
-                cc = c.execute("SELECT * FROM MSG WHERE ID=?", (event.messageId,))
-                for x in cc:
-                    msg = x[1]
-                msg = re.sub('@', '\@', msg)
-                await helper.dc_debug_webhook(debug_webhook_link, f'{event.authorId} 撤回了一条消息： {msg}', '[QQ]')
-            except Exception:
-                traceback.print_exc()
+    try:
+        eventlet.monkey_patch()
+        with eventlet.Timeout(15):
+            if event.group.id == target_qqgroup:
+                dst = {}
+                if event.authorId != qq:
+                    dst['Type'] = 'QQrecall'
+                else:
+                    dst['Type'] = 'QQrecallI'
+                dst['MID'] = event.messageId
+                dst['UID'] = event.authorId
+                j = json.dumps(dst)
+                await helper.sendtoWebsocket(websocket_port, j)
+                if debug:
+                    print(event.authorId)
+                    try:
+                        c = helper.connect_db('./qqmsg.db')
+                        cc = c.execute("SELECT * FROM MSG WHERE ID=?", (event.messageId,))
+                        for x in cc:
+                            msg = x[1]
+                        msg = re.sub('@', '\@', msg)
+                        await helper.dc_debug_webhook(debug_webhook_link, f'{event.authorId} 撤回了一条消息： {msg}', '[QQ]')
+                    except Exception:
+                        traceback.print_exc()
+    except eventlet.TimeoutError:
+        traceback.print_exc()
 
 
 @bcc.receiver("GroupMessage")
 async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if group.id == target_qqgroup:
-        if message.asDisplay() == '$count':
-            a = helper.connect_db('./msgid.db').execute('SELECT COUNT(*) as cnt FROM ID').fetchone()
-            a1 = round(os.path.getsize('./msgid.db')/ float(1024*1024), 2)
-            b = helper.connect_db('./qqmsg.db').execute('SELECT COUNT(*) as cnt FROM MSG').fetchone()
-            b1 = round(os.path.getsize('./qqmsg.db') / float(1024 * 1024), 2)
-            c = helper.connect_db('./dcname.db').execute('SELECT COUNT(*) as cnt FROM DCNAME').fetchone()
-            c1 = round(os.path.getsize('./dcname.db')/float(1024*1024), 2)
-            d = f'''msgid.db({a1}MB):
+    try:
+        eventlet.monkey_patch()
+        with eventlet.Timeout(15):
+            if group.id == target_qqgroup:
+                if message.asDisplay() == '$count':
+                    a = helper.connect_db('./msgid.db').execute('SELECT COUNT(*) as cnt FROM ID').fetchone()
+                    a1 = round(os.path.getsize('./msgid.db')/ float(1024*1024), 2)
+                    b = helper.connect_db('./qqmsg.db').execute('SELECT COUNT(*) as cnt FROM MSG').fetchone()
+                    b1 = round(os.path.getsize('./qqmsg.db') / float(1024 * 1024), 2)
+                    c = helper.connect_db('./dcname.db').execute('SELECT COUNT(*) as cnt FROM DCNAME').fetchone()
+                    c1 = round(os.path.getsize('./dcname.db')/float(1024*1024), 2)
+                    d = f'''msgid.db({a1}MB):
 - ID: {a[0]}
 qqmsg.db({b1}MB):
 - MSG: {b[0]}
 dcname.db({c1}MB):
 - DCNAME: {c[0]}'''
-            await app.sendGroupMessage(group, MessageChain.create([Plain(d)]))
-        if message.asDisplay() == '谁At我':
-            try:
-                if debug == True:
-                    a = helper.connect_db('./qqmsg.db').execute(f"SELECT ID, MSG FROM MSG WHERE MSG LIKE '%{'@[QQ: ' + str(member.id) + ']'}%'").fetchall()[-1]
-                    print(a[0])
-                    await app.sendGroupMessage(group, MessageChain.create([Plain('This.')]), quote=int(a[0]))
-            except Exception:
-                await app.sendGroupMessage(group, MessageChain.create([Plain('无法定位。')]))
+                    await app.sendGroupMessage(group, MessageChain.create([Plain(d)]))
+                if message.asDisplay() == '谁At我':
+                    try:
+                        if debug == True:
+                            a = helper.connect_db('./qqmsg.db').execute(f"SELECT ID, MSG FROM MSG WHERE MSG LIKE '%{'@[QQ: ' + str(member.id) + ']'}%'").fetchall()[-1]
+                            print(a[0])
+                            await app.sendGroupMessage(group, MessageChain.create([Plain('This.')]), quote=int(a[0]))
+                    except Exception:
+                        await app.sendGroupMessage(group, MessageChain.create([Plain('无法定位。')]))
+    except eventlet.TimeoutError:
+        traceback.print_exc()
 
 
 app.launch_blocking()
--- helper.py
+++ helper.py
@@ -1,8 +1,11 @@
 import os
 import sqlite3
+import traceback
+
 import aiohttp
 from discord import Webhook, AsyncWebhookAdapter
 import websockets
+import eventlet
 
 def writeid(dcmsgid, qqmsgid):
     dbpath = os.path.abspath('./msgid.db')
@@ -24,6 +27,13 @@ def writeid(dcmsgid, qqmsgid):
         c.execute("INSERT INTO ID (DCID, QQID) VALUES (?, ?)", (dcmsgid, qqmsgid))
     conn.commit()
 
+
+def delid(qqmsgid):
+    dbpath = os.path.abspath('./msgid.db')
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    c.execute("DElETE FROM ID WHERE QQID=?", (qqmsgid,))
+    conn.commit()
 
 def writeqqmsg(msgid, msg):
     dbpath = os.path.abspath('./qqmsg.db')
@@ -69,16 +79,25 @@ def connect_db(path):
 
 
 async def dc_debug_webhook(debug_webhook_link, message, username, avatar_url=None):
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(5)) as session:
-        webhook = Webhook.from_url(debug_webhook_link
-                                   ,
-                                   adapter=AsyncWebhookAdapter(session))
-        await webhook.send(message, username=username,
-                           avatar_url=avatar_url)
+    eventlet.monkey_patch()
+    try:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(15)) as session:
+            webhook = Webhook.from_url(debug_webhook_link
+                                       ,
+                                       adapter=AsyncWebhookAdapter(session))
+            await webhook.send(message, username=username,
+                               avatar_url=avatar_url)
+            await session.close()
+    except eventlet.TimeoutError:
+        traceback.print_exc()
 
 
 async def sendtoWebsocket(websocket_port, text):
-    async with websockets.connect('ws://127.0.0.1:' + websocket_port) as websocket:
-        await websocket.send(text)
-        await websocket.close()
-
+    eventlet.monkey_patch()
+    try:
+        with eventlet.Timeout(15):
+            async with websockets.connect('ws://127.0.0.1:' + websocket_port) as websocket:
+                await websocket.send(text)
+                await websocket.close()
+    except eventlet.TimeoutError:
+        traceback.print_exc()
